e2605b74a5a476c44b02a91f4ed0e6cbd227acbb
Igor Cheledinov <cheledinov@gmail.com>
2016-04-01 01:18:42 +0300

    Adds PSR-4 namespace for app.

--- src/PhpOfByBundle/PhpOfByBundle.php
+++ src/PhpOfByBundle/PhpOfByBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PhpOfByBundle;
+namespace PhpOfBy\PhpOfByBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
--- src/PhpOfByBundle/Tests/Controller/DefaultControllerTest.php
+++ src/PhpOfByBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PhpOfByBundle\Tests\Controller;
+namespace PhpOfBy\PhpOfByBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
