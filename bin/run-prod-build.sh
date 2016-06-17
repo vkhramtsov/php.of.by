04a97b836cebb6730ccb1a58430827cbfa3b14f1
Vladimir Khramtsov <vkhramtsov@gmail.com>
2016-06-18 00:28:30 +0300

    Use script provider for deploy. Fix .travis

--- bin/run-prod-build.sh
+++ bin/run-prod-build.sh
@@ -1,2 +1,13 @@
-#!/bin/sh
-export SYMFONY_ENV=prod && php phing.phar build-prod && export SYMFONY_ENV=
+#!/usr/bin/env sh
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    export SYMFONY_ENV=prod && php phing.phar build-prod && export SYMFONY_ENV=
+    eval "$(ssh-agent -s)"
+    chmod 600 .travis/deploy.key
+    ssh-add .travis/deploy.key
+    sh ./bin/run-prod-build.sh
+    mkdir build
+    mv * build
+    tar -czf package.tgz build
+    scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+    ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
+fi
