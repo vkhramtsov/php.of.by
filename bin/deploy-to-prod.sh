15bc468556602169e48a9922dc55cc9672b53bdf
Vladimir Khramtsov <vkhramtsov@gmail.com>
2016-06-22 00:26:55 +0300

    Move creating package to before_deploy step

--- bin/deploy-to-prod.sh
+++ bin/deploy-to-prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # We should deploy only build for php 7
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ $(phpenv version-name) = "7.0" ]; then
-    export SYMFONY_ENV=prod && php phing.phar build-prod && export SYMFONY_ENV=
+    #export SYMFONY_ENV=prod && php phing.phar build-prod && export SYMFONY_ENV=
     eval "$(ssh-agent -s)"
     chmod 600 .travis/deploy.key
     ssh-add .travis/deploy.key
