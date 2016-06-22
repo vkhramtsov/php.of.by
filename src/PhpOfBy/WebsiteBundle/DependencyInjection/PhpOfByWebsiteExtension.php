77191f7379640f637ce3ad1b9571bf3d89037297
Vladimir Khramtsov <vkhramtsov@gmail.com>
2016-06-22 21:18:00 +0300

    Change name for website bundle

--- src/PhpOfBy/WebsiteBundle/DependencyInjection/PhpOfByWebsiteExtension.php
+++ src/PhpOfBy/WebsiteBundle/DependencyInjection/PhpOfByWebsiteExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class WebsiteExtension extends Extension
+class PhpOfByWebsiteExtension extends Extension
 {
     /**
      * {@inheritdoc}
