c4ba113b82585bbbad9a352932b5957c99621ce7
Vladimir Khramtsov <vkhramtsov@gmail.com>
2016-08-04 00:31:01 +0300

    Update deployment process. Build package with phing

--- bin/deploy-to-prod.sh
+++ bin/deploy-to-prod.sh
@@ -4,9 +4,6 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ $(phpenv version-name) = "7.0" ];
     eval "$(ssh-agent -s)"
     chmod 600 .travis/deploy.key
     ssh-add .travis/deploy.key
-    mkdir build
-    mv * build
-    tar -czf package.tgz build
     scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
     ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
 fi
