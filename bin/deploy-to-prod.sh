67b71799b19d736c7b4ece77544c89e87ac1f6ad
Vladimir Khramtsov <vkhramtsov@gmail.com>
2016-08-28 23:47:16 +0300

    Fix DB deploy command line

--- bin/deploy-to-prod.sh
+++ bin/deploy-to-prod.sh
@@ -6,5 +6,6 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ $(phpenv version-name) = "7.0" ];
     ssh-add .travis/deploy.key
     scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
     ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
-    ssh $DEPLOY_USER@$DEPLOY_HOST export\ SYMFONY_ENV=prod\ &&\ php\ ~/php.of.by/builds/current/bin/console\ doctrine:migrations:migrate
+    ssh $DEPLOY_USER@$DEPLOY_HOST export\ SYMFONY_ENV=prod
+    ssh $DEPLOY_USER@$DEPLOY_HOST php\ ~/php.of.by/builds/current/bin/console\ doctrine:migrations:migrate
 fi
