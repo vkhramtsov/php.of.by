9b1db7ff7531894a6a4c225d05d0ba72da8fa854
Vladimir Khramtsov <vkhramtsov@gmail.com>
2018-06-27 23:55:10 +0300

    Move public dir back to web

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     // the project directory where compiled assets will be stored
-    .setOutputPath('public/')
+    .setOutputPath('web/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/')
     //.cleanupOutputBeforeBuild()
