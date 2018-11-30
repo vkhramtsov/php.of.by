3fa84f44515bb1e4fce025bdeb1c85a974d3d32b
Vladimir Khramtsov <vkhramtsov@gmail.com>
2018-11-30 19:18:01 +0300

    Fix creating/editing articles

--- src/PhpOfBy/WebsiteBundle/Controller/DefaultController.php
+++ src/PhpOfBy/WebsiteBundle/Controller/DefaultController.php
@@ -6,9 +6,6 @@ use CommonBundle\Controller\AbstractServiceController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Mvc;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Mvc\Route(service="phpofby_website.controller.default")
- */
 class DefaultController extends AbstractServiceController
 {
     /**
