0a1cbba3bc2007b80e9c0e3393e38b8403318fde
Vladimir Khramtsov <vkhramtsov@gmail.com>
2021-02-03 11:36:56 +0300

    Upgrade phpcs

--- src/PhpOfBy/SecurityBundle/DependencyInjection/PhpOfBySecurityExtension.php
+++ src/PhpOfBy/SecurityBundle/DependencyInjection/PhpOfBySecurityExtension.php
@@ -19,7 +19,7 @@ class PhpOfBySecurityExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
     }
 }
